f8397cfaacb60304aec172e717e3e17318a380de
chiefsmurph <chiefsmurph@gmail.com>
2020-05-18 22:44:27 -0700

    lets try this

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const socketIOClient = require('socket.io-client');
 const io = require('socket.io')();
 const { pick } = require('underscore')
-const { rhEndpoint } = require('./config');
+const { rhEndpoint, options } = require('./config');
 
-const rhSocket = socketIOClient(rhEndpoint);
-
+const rhSocket = socketIOClient(rhEndpoint, options);
+const toPercents = require('./utils/to-percents');
 
 let curAppState = {};
 
 io.on('connection', client => {
   console.log('new client connected incoming...');
-  emitAppState(client);
+  emitChartData(client);
 });
 io.listen(3001);
 
-const emitAppState = (socket = io) => 
-  socket && socket.emit('server:data-update', curAppState);
+const emitChartData = (socket = io) => 
+  socket && socket.emit('server:stock-data', toPercents(curAppState.balanceReports));
 
 
 rhSocket.on('server:data-update', data => {
-  // console.log('data update has been received from rhSocket');
 
   const nextAppState = {
     ...curAppState,
-    ...pick(data, ['derivedCollections', 'lastCollectionRefresh'])
+    ...pick(data, ['balanceReports'])
   };
 
   if (JSON.stringify(curAppState) !== JSON.stringify(nextAppState)) {
     console.log('app state has been updated from rhSocket');
     curAppState = nextAppState;
-    emitAppState(); // to all
+    emitChartData(); // to all
   }
+});
+
+rhSocket.on('server:balance-report', ({ report }) => {
+  curAppState.balanceReports.push(report);
+  emitChartData(); // to all
 });
---
+++ utils/to-percents.js
@@ -0,0 +1,31 @@
+const { uniq } = require('underscore');
+const getTrend = require('./get-trend');
+
+module.exports = (balanceReports = []) => {
+
+  if (!balanceReports || !balanceReports.length) return [];
+
+  let copy = [...balanceReports];
+  copy = copy.map(report => ({
+    ...report,
+    date: (new Date(report.time)).toLocaleDateString()
+  }));
+
+  const mostRecent = copy.filter(r => r.isRegularHours).pop().date;
+
+  const prevClose = copy.slice().reverse().find(r => r.date !== mostRecent && r.isRegularHours);
+
+
+
+  const onlyToday = copy.filter(r => r.date === mostRecent && r.isRegularHours);
+
+  const percs = onlyToday.map(r => ({
+    sp500: getTrend(r.indexPrices.sp500, prevClose.indexPrices.sp500),
+    nasdaq: getTrend(r.indexPrices.nasdaq, prevClose.indexPrices.nasdaq),
+    russell2000: getTrend(r.indexPrices.russell2000, prevClose.indexPrices.russell2000),
+    alpacaBalance: getTrend(r.alpacaBalance, prevClose.alpacaBalance),
+    time: r.time
+  }));
+
+  return percs;
+};
