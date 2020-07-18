b802d46073ab4eaae9a79fc6281ca059a0a0ce87
chiefsmurph <chiefsmurph@gmail.com>
2020-07-18 14:35:20 -0700

    tea

--- utils/get-recommendations.js
+++ utils/get-recommendations.js
@@ -1,6 +1,6 @@
 const getRecommendations = (positions = []) => {
   return positions
-    .filter(p => p.zScoreFinal)
+    .filter(p => p.zScoreFinal && p.scan)
     .sort((a, b) => b.zScoreFinal - a.zScoreFinal)
     .slice(0, 3)
     .map(p => p.ticker);
